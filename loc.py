e2b11155b79b8c0cc1d2a5a97f7b7ebcd3061d8a
peterchylewski <peter@boring.ch>
2014-06-13 03:14:03 +0200

    foobar

--- loc.py
+++
@@ -1,5 +0,0 @@
-import location
-
-#device = location.GPSDevice()
-
-print location.GPS_DEVICE_LATLONG_SET
--- location.py
+++
@@ -1,6 +0,0 @@
-import location
-
-control = location.GPSDControl.get_default()
-device = location.GPSDevice()
-
-#print location.GPS_DEVICE_LATLONG_SET
